87dd1bc4bf068de44a849aaa1ddc90a27e7b7e15
Ertiosanluci <160151485+Ertiosanluci@users.noreply.github.com>
2025-05-14 14:02:50 +0200

    nueva configuración para web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2160998023"
+    serviceWorkerVersion: "2639336639"
   }
 });
