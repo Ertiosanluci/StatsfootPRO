448234c8abf07ea59400503278eff828fd11def3
Ertiosanluci <160151485+Ertiosanluci@users.noreply.github.com>
2025-05-14 13:56:37 +0200

    nueva build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d4c3a59ee5d8cf80df7a972451b58acd
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "63ecb3418810dc549680804e88a7134e",
+"flutter_bootstrap.js": "42a7adea958961c8f36251583b68f6e2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
